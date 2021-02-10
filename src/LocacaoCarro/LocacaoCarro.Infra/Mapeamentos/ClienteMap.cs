84bbe43b4bfa814282575ed7bb2d95a6e54bdedd
Eduardo Samaan <eduardo.samaan@localiza.com>
2021-02-10 19:16:16 -0300

    Mapeamento entidade domínio <-> entidade DB.

---
+++ src/LocacaoCarro/LocacaoCarro.Infra/Mapeamentos/ClienteMap.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using LocacaoCarro.Dominio.Entidades;
+using LocacaoCarro.Dominio.ObjetosValor;
+using LocacaoCarro.Infra.BDModelos;
+
+namespace LocacaoCarro.Infra.Mapeamentos
+{
+    public class ClienteMap : Profile
+    {
+        public ClienteMap()
+        {
+            CreateMap<ClienteBDModelo, Cliente>()
+                .ForMember(dest => dest.Nome.PrimeiroNome, m => m.MapFrom(src => src.Nome))
+                .ForMember(dest => dest.Nome.Sobrenome, m => m.MapFrom(src => src.Sobrenome))
+                .ForMember(dest => dest.HashSenha, m => m.MapFrom(src => src.HashSenha))
+                .ForMember(dest => dest.Cpf, m => m.MapFrom(src => src.Cpf))
+                .ForMember(dest => dest.Endereco.Cep, m => m.MapFrom(src => src.Cep))
+                .ForMember(dest => dest.Endereco.Logradouro, m => m.MapFrom(src => src.Logradouro))
+                .ForMember(dest => dest.Endereco.Numero, m => m.MapFrom(src => src.Numero))
+                .ForMember(dest => dest.Endereco.Complemento, m => m.MapFrom(src => src.Complemento))
+                .ForMember(dest => dest.Endereco.Cidade, m => m.MapFrom(src => src.Cidade))
+                .ForMember(dest => dest.Endereco.Estado, m => m.MapFrom(src => src.Estado))
+                .ForMember(dest => dest.Aniversario, m => m.MapFrom(src => src.Aniversario))
+                .ConstructUsing(src =>
+                    new Cliente(
+                        new Nome(src.Nome, src.Sobrenome),
+                        new Cpf(src.Cpf),
+                        new Endereco(src.Cep, src.Logradouro, src.Numero, src.Complemento, src.Cidade, src.Estado),
+                        src.Aniversario,
+                        src.HashSenha)
+                    );
+        }
+    }
+}
---
+++ src/LocacaoCarro/LocacaoCarro.Infra/Mapeamentos/OperadorMap.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using LocacaoCarro.Dominio.Entidades;
+using LocacaoCarro.Infra.BDModelos;
+
+namespace LocacaoCarro.Infra.Mapeamentos
+{
+    public class OperadorMap : Profile
+    {
+        public OperadorMap()
+        {
+            CreateMap<OperadorBDModelo, Operador>()
+                .ForMember(dest => dest.Nome.PrimeiroNome, m => m.MapFrom(src => src.Nome))
+                .ForMember(dest => dest.Nome.Sobrenome, m => m.MapFrom(src => src.Sobrenome))
+                .ForMember(dest => dest.HashSenha, m => m.MapFrom(src => src.HashSenha))
+                .ForMember(dest => dest.Matricula, m => m.MapFrom(src => src.Matricula))
+                .ConstructUsing(src =>
+                    new Operador(
+                        new Matricula(src.Matricula),
+                        new Nome(src.Nome, src.Sobrenome),
+                        src.HashSenha)
+                    );
+        }
+    }
+}
