d81f75016e4a5c8aeaafead2aff038ce3e9682cd
Eduardo Samaan <eduardo.samaan@localiza.com>
2021-02-10 20:02:40 -0300

    Classe para padronização dos resultados da camada de aplicação.

---
+++ src/LocacaoCarro/LocacaoCarro.Aplicacao/Resultados/Resultado.cs
@@ -0,0 +1,57 @@
+﻿using Flunt.Notifications;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LocacaoCarro.Aplicacao.Resultados
+{
+    public class Resultado : Notifiable
+    {
+        public bool Success { get { return !Notifications.Any(); } }
+
+        protected Resultado()
+        {
+        }
+
+        protected Resultado(IReadOnlyCollection<Notification> notifications)
+        {
+            AddNotifications(notifications);
+        }
+
+        public static Resultado Ok()
+        {
+            return new Resultado();
+        }
+
+        public static Resultado Erro(IReadOnlyCollection<Notification> notifications)
+        {
+            return new Resultado(notifications);
+        }
+    }
+
+    public class Resultado<T> : Notifiable where T : class
+    {
+        public bool Sucesso { get { return !Notifications.Any(); } }
+        public T Objeto { get; }
+
+        private Resultado(T obj)
+        {
+            Objeto = obj;
+        }
+
+        private Resultado(IReadOnlyCollection<Notification> notifications)
+        {
+            Objeto = null;
+            AddNotifications(notifications);
+        }
+
+        public static Resultado<T> Ok(T obj)
+        {
+            return new Resultado<T>(obj);
+        }
+
+        public static Resultado<T> Erro(IReadOnlyCollection<Notification> notifications)
+        {
+            return new Resultado<T>(notifications);
+        }
+    }
+}
